88e340892e0d33fdda87ac4c4dc29c77fd0a453d
Philippe Kurz <philippe.kurz@gmail.com>
2025-05-05 14:09:25 +0200

    premier

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+            'name' => 'Admin',
+            'prenom' => 'Admin',
+            'pseudo' => 'admin',
+            'email' => 'admin@gmail.com',
+            'is_admin' => true,
+            'password' => bcrypt('admin')
+        ]);
+
+        User::create([
+            'name' => 'User',
+            'prenom' => 'User',
+            'pseudo' => 'user',
+            'email' => 'user@gmail.com',
+            'password' => bcrypt('user')
+        ]);
+
+        User::factory(100)->create();
+
+    }
+}
